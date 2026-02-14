2961d25d4b3dd7376df5d8686a101cd84f4ce675
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2026-02-14 16:34:40 -0600

    init

---
+++ src/script.js
@@ -0,0 +1,48 @@
+const navActivity = document.getElementById("nav_activity");
+const navLeaderboard = document.getElementById("nav_leaderboard");
+const navGroup = document.getElementById("nav_group");
+const navSettings = document.getElementById("nav_settings");
+
+const tabActivity = document.getElementById("tab_activity");
+const tabLeaderboard = document.getElementById("tab_leaderboard");
+const tabGroup = document.getElementById("tab_group");
+const tabSettings = document.getElementById("tab_settings");
+
+let currentNav = navActivity;
+let currentTab = tabActivity;
+
+navActivity.addEventListener("click", () => {
+    currentNav.classList.remove("active");
+    currentTab.classList.remove("active");
+    navActivity.classList.add("active");
+    tabActivity.classList.add("active");
+    currentNav = navActivity;
+    currentTab = tabActivity;
+});
+
+navLeaderboard.addEventListener("click", () => {
+    currentNav.classList.remove("active");
+    currentTab.classList.remove("active");
+    navLeaderboard.classList.add("active");
+    tabLeaderboard.classList.add("active");
+    currentNav = navLeaderboard;
+    currentTab = tabLeaderboard;
+});
+
+navGroup.addEventListener("click", () => {
+    currentNav.classList.remove("active");
+    currentTab.classList.remove("active");
+    navGroup.classList.add("active");
+    tabGroup.classList.add("active");
+    currentNav = navGroup;
+    currentTab = tabGroup;
+});
+
+navSettings.addEventListener("click", () => {
+    currentNav.classList.remove("active");
+    currentTab.classList.remove("active");
+    navSettings.classList.add("active");
+    tabSettings.classList.add("active");
+    currentNav = navSettings;
+    currentTab = tabSettings;
+});
